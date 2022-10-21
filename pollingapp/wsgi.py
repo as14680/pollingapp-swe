8f52b65723efe4e8c43c5818d775d0cc5c4f383c
akshat <akshat.sharma@nyu.edu>
2022-10-21 02:47:45 -0400

    black and flake update

--- pollingapp/wsgi.py
+++ pollingapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollingapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pollingapp.settings")
 
 application = get_wsgi_application()
